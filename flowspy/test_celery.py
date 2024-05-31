68c96d77afa25115e89635cc5a4a090368c973d5
root <root@remedios-pre.redimadrid.es>
2024-05-31 10:02:05 +0200

    Cambiar la frecuencia de ejecución de la tarea route_sync a la establecida

---
+++ flowspy/test_celery.py
@@ -0,0 +1,29 @@
+from __future__ import absolute_import, unicode_literals
+
+import os
+
+#from django.conf import settings
+from flowspy import settings
+from celery import Celery
+
+from celery.schedules import crontab
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flowspy.settings')
+
+
+#using celerys broker or redis
+app = Celery('flowspy')
+#app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings')
+app.conf.timezone = 'UTC'
+app.conf.redbeat_redis_url = 'redis://redis:6379'
+app.autodiscover_tasks()
+app.conf.beat_schedule = {
+    "every-day-route-sync": {
+        "task": "flowspec.tasks.routes_sync",
+        "schedule": crontab(minute='*/5', hour='*'),
+        "args": (),
+    },
+}
+
